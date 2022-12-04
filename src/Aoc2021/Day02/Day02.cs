1ab59091d6a08c055436dae38bd81e1e2982db02
Stone C. Lasley <stonelasley@gmail.com>
2022-12-04 13:42:17 -0700

    Day 2021/day02

---
+++ src/Aoc2021/Day02/Day02.cs
@@ -0,0 +1,65 @@
+﻿namespace Aoc2021.Day02;
+
+public class Day02 : IDay
+{
+    public int SolveOne(IInputProvider inputProvider)
+    {
+        IEnumerable<KeyValuePair<string, int>> lines = inputProvider.Read().Select(line =>
+        {
+            var pair = line.Split(' ');
+            return new KeyValuePair<string, int>(pair[0], Int32.Parse(pair[1]));
+        });
+
+        int x = 0;
+        int y = 0;
+
+        foreach (var kvp in lines)
+        {
+            switch (kvp.Key)
+            {
+                case "up":
+                    y -= kvp.Value;
+                    break;
+                case "down":
+                    y += kvp.Value;
+                    break;
+                default:
+                    x += kvp.Value;
+                    break;
+            }
+        }
+        return x*y;
+    }
+
+    public int SolveTwo(IInputProvider inputProvider)
+    {
+        IEnumerable<KeyValuePair<string, int>> lines = inputProvider.Read().Select(line =>
+        {
+            var pair = line.Split(' ');
+            return new KeyValuePair<string, int>(pair[0], Int32.Parse(pair[1]));
+        });
+
+        int x = 0;
+        int y = 0;
+        int aim = 0;
+
+
+        foreach (var kvp in lines)
+        {
+            switch (kvp.Key)
+            {
+                case "up":
+                    aim -= kvp.Value;
+                    break;
+                case "down":
+                    aim += kvp.Value;
+                    break;
+                default:
+                    x += kvp.Value;
+                    y += aim * kvp.Value;
+                    break;
+            }
+        }
+        return x*y;
+    }
+}
