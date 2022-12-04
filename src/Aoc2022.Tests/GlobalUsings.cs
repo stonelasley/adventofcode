5580dfe8bc4bc5c6827f882c61efc86e357852f1
Stone C. Lasley <stonelasley@gmail.com>
2022-12-04 13:29:20 -0700

    tests(2022/Day01): update for windows newlines

--- src/Aoc2022.Tests/GlobalUsings.cs
+++ src/Aoc2022.Tests/GlobalUsings.cs
@@ -3,5 +3,6 @@ global using Moq;
 global using Xunit;
 global using System.Collections.Generic;
 global using System.Linq;
+global using System;
 global using Aoc.Core;
 global using Aoc.Core.Tests;
