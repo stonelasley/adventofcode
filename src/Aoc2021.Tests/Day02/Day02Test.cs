ba2970f2151683bd7aa3ec24d6912a4dfbb76cfe
Stone C. Lasley <stonelasley@gmail.com>
2022-12-04 14:00:16 -0700

    test(2021): fix missing test body

--- src/Aoc2021.Tests/Day02/Day02Test.cs
+++ src/Aoc2021.Tests/Day02/Day02Test.cs
@@ -38,22 +38,22 @@ public class Day02Test : BaseTest<Day02>
         [Fact]
         public void ShouldSolve()
         {
-            InputProvider.Setup(x => x.Read()).Returns(new []
-            {
-                "199",
-                "200",
-                "208",
-                "210",
-                "200",
-                "207",
-                "240",
-                "269",
-                "260",
-                "263"
-            });
+            InputProvider
+               .Setup(x => x.Read())
+               .Returns(
+                    new[]
+                    {
+                        "forward 5",
+                        "down 5",
+                        "forward 8",
+                        "up 3",
+                        "down 8",
+                        "forward 2"
+                    }
+                );
 
             var actual = Sut.SolveTwo(InputProvider.Object);
-            actual.Should().Be(5);
+            actual.Should().Be(900);
         }
     }
 }
