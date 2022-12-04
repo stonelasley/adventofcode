420f5c3deb4650acdb5fb35459c0a1254adda17f
Stone C. Lasley <stonelasley@gmail.com>
2022-12-04 13:51:50 -0700

    2021/Day03

---
+++ src/Aoc2021.Tests/Day03/Day03Test.cs
@@ -0,0 +1,71 @@
+namespace Aoc2021.Tests.Day03;
+
+using Aoc2021.Day03;
+
+public class Day03Test : BaseTest<Day03>
+{
+    public Day03Test()
+    {
+        Sut = new Day03();
+    }
+
+    public class SolveOne : Day03Test
+    {
+        [Fact]
+        public void ShouldSolve()
+        {
+            InputProvider
+                .Setup(x => x.Read())
+                .Returns(
+                    new[]
+                    {
+                        "00100",
+                        "11110",
+                        "10110",
+                        "10111",
+                        "10101",
+                        "01111",
+                        "00111",
+                        "11100",
+                        "10000",
+                        "11001",
+                        "00010",
+                        "01010"
+                    }
+                );
+
+            var actual = Sut.SolveOne(InputProvider.Object);
+            actual.Should().Be(198);
+        }
+    }
+
+    public class SolveTwo : Day03Test
+    {
+        [Fact]
+        public void ShouldSolve()
+        {
+            InputProvider
+               .Setup(x => x.Read())
+               .Returns(
+                    new[]
+                    {
+                        "00100",
+                        "11110",
+                        "10110",
+                        "10111",
+                        "10101",
+                        "01111",
+                        "00111",
+                        "11100",
+                        "10000",
+                        "11001",
+                        "00010",
+                        "01010"
+                    }
+                );
+
+            var actual = Sut.SolveTwo(InputProvider.Object);
+            actual.Should().Be(230);
+        }
+    }
+}
