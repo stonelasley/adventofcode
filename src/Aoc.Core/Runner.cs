b6c4b849af358353cf5e50aaef2b217470ed3559
Stone C. Lasley <stone@lasley.io>
2023-12-04 16:22:03 -0700

    feat(Runner.cs): add runtime to output

--- src/Aoc.Core/Runner.cs
+++ src/Aoc.Core/Runner.cs
@@ -18,11 +18,11 @@ public static class Runner
             {
                 day = (IDay?)
                     Activator
-                        .CreateInstance(
+                       .CreateInstance(
                             assembly.GetName().Name!,
                             $"{assembly.GetName().Name}.{dayName}.{dayName}"
                         )
-                        ?.Unwrap();
+                      ?.Unwrap();
             }
             catch (Exception)
             {
@@ -31,9 +31,14 @@ public static class Runner
                 Console.WriteLine("Part Two: Unsolved");
                 continue;
             }
-
-            Console.WriteLine($"Part One: {day!.SolveOne(new InputReader(dayName))}");
-            Console.WriteLine($"Part Two: {day!.SolveTwo(new InputReader(dayName))}");
+            Stopwatch sw = Stopwatch.StartNew();
+            string partOneResult = day!.SolveOne(new InputReader(dayName));
+            sw.Stop();
+            Console.WriteLine($"Part One: {partOneResult} duration: {sw.Elapsed}");
+            sw = Stopwatch.StartNew();
+            string partTwoResult = day!.SolveTwo(new InputReader(dayName));
+            sw.Stop();
+            Console.WriteLine($"Part Two: {partTwoResult} duration: {sw.Elapsed}");
         }
     }
 }
