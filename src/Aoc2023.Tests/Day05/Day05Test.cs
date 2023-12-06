35a2b6b79717e8f04ec61e5e2f3b63fb4966b687
Stone C. Lasley <stone@lasley.io>
2023-12-05 17:12:16 -0700

    feat(2023): Day 5 part 1

--- src/Aoc2023.Tests/Day05/Day05Test.cs
+++ src/Aoc2023.Tests/Day05/Day05Test.cs
@@ -11,9 +11,44 @@ public class Day05Test : BaseTest<Day05>
         [Fact]
         public void ShouldSolve()
         {
-            InputProvider.Setup(x => x.Read()).Returns(new[] { "", "", });
+            InputProvider.Setup(x => x.Read()).Returns(new[]
+            {
+                "seeds: 79 14 55 13",
+                "",
+                "seed-to-soil map:",
+                "50 98 2",
+                "52 50 48",
+                "",
+                "soil-to-fertilizer map:",
+                "0 15 37",
+                "37 52 2",
+                "39 0 15",
+                "",
+                "fertilizer-to-water map:",
+                "49 53 8",
+                "0 11 42",
+                "42 0 7",
+                "57 7 4",
+                "",
+                "water-to-light map:",
+                "88 18 7",
+                "18 25 70",
+                "",
+                "light-to-temperature map:",
+                "45 77 23",
+                "81 45 19",
+                "68 64 13",
+                "",
+                "temperature-to-humidity map:",
+                "0 69 1",
+                "1 0 69",
+                "",
+                "humidity-to-location map:",
+                "60 56 37",
+                "56 93 4"
+            });
             string result = Sut.SolveOne(InputProvider.Object);
-            result.Should().Be("unsolved");
+            result.Should().Be("35");
         }
     }
 
@@ -27,4 +62,61 @@ public class Day05Test : BaseTest<Day05>
             result.Should().Be("unsolved");
         }
     }
+}
+
+public class MapperTest
+{
+
+    [Fact]
+    public void ShouldInitialize()
+    {
+        string[] input =
+        {
+            "seed-to-soil map:",
+            "50 98 2",
+            "52 50 48 "
+        };
+
+        Mapper actual = new(input);
+        actual.Name.Should().Be("seed-to-soil map:");
+
+
+    }
+
+    [Theory]
+    [InlineData(79, 81)]
+    [InlineData(14, 14)]
+    [InlineData(55, 57)]
+    [InlineData(13, 13)]
+    public void ShouldMap(int seed, int expectedSoil)
+    {
+        string[] input =
+        {
+            "seed-to-soil map:",
+            "50 98 2",
+            "52 50 48 "
+        };
+
+        Mapper m = new(input);
+        m.Map(seed).Should().Be(expectedSoil);
+
+
+    }
+
+}
+
+public class MapSpecTest
+{
+
+    [Theory]
+    [InlineData("50 98 2", 50)]
+    [InlineData("52 50 48 ", 52)]
+    public void ShouldInitialize(string input, int expectedDestination)
+    {
+        MapSpec actual = new(input);
+        actual.DestinationStart.Should().Be(expectedDestination);
+
+
+    }
+
 }
--- src/Aoc2023/Day05/Day05.cs
+++ src/Aoc2023/Day05/Day05.cs
@@ -5,7 +5,37 @@ public class Day05 : IDay
     public string SolveOne(IInputProvider inputProvider)
     {
         var input = inputProvider.Read();
-        return "unsolved";
+        long[] seeds = ParseSeeds(input[0]);
+        Dictionary<string, Mapper> mappers = new();
+
+        List<string> mapperInput = new();
+        for (int i = 2; i < input.Length - 2; i++)
+        {
+            string inputLine = input[i];
+            if (string.IsNullOrEmpty(inputLine))
+            {
+                Mapper mapper = new(mapperInput.ToArray());
+                mappers.Add(mapper.Name, mapper);
+                mapperInput = new();
+                continue;
+            }
+            mapperInput.Add(inputLine);
+        }
+        var mL = new Mapper(mapperInput.ToArray());
+        mappers.Add(mL.Name, mL);
+
+        return seeds.Select(seed =>
+        {
+            var soil = mappers["seed-to-soil map:"].Map(seed);
+            var fertilizer = mappers["soil-to-fertilizer map:"].Map(soil);
+            var water = mappers["fertilizer-to-water map:"].Map(fertilizer);
+            var light = mappers["water-to-light map:"].Map(water);
+            var temp = mappers["light-to-temperature map:"].Map(light);
+            var humidity = mappers["temperature-to-humidity map:"].Map(temp);
+            return mappers["humidity-to-location map:"].Map(humidity);
+
+        }).Min().ToString();
+
     }
 
     public string SolveTwo(IInputProvider inputProvider)
@@ -13,4 +43,67 @@ public class Day05 : IDay
         var input = inputProvider.Read();
         return "unsolved";
     }
+
+    long[] ParseSeeds(string input) => Regex
+                                      .Matches(input, @"(\d+)")
+                                      .Select(x => long.Parse(x.Groups[1].Value)).ToArray();
 }
+
+public class Mapper
+{
+    public string Name { get; set; }
+    public MapSpec[] Specs;
+
+    public Mapper(string[] mappingInput)
+    {
+        Name = mappingInput[0];
+        Specs = new ArraySegment<string>(mappingInput, 1, mappingInput.Length - 1)
+                .Select(x => new MapSpec(x))
+                .ToArray();
+    }
+
+    public long Map(long source)
+    {
+        MapSpec? spec = Specs.FirstOrDefault(x => x.HasMap(source));
+        return spec?.Map(source) ?? source;
+    }
+}
+
+public class MapSpec
+{
+    private long[] values;
+    public MapSpec(string input)
+    {
+        values = input.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                      .Select(long.Parse)
+                      .ToArray();
+
+        DestinationStart = values[0];
+        SourceStart = values[1];
+        Length = values[2];
+
+        SourceEnd = SourceStart + Length;
+        DestinationEnd = DestinationStart + Length;
+
+    }
+    public long DestinationStart { get; }
+    public long DestinationEnd { get; }
+    public long SourceStart { get; }
+    public long SourceEnd { get; }
+    public long Length { get; }
+
+    public bool HasMap(long num)
+    {
+        return num >= SourceStart && num <= SourceEnd;
+    }
+    
+    public long Map(long num)
+    {
+        if (!HasMap(num)) return num;
+        long offSet = num - SourceStart;
+        return DestinationStart + offSet;
+
+    }
+
+
+};
